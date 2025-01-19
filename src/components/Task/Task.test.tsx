3f75b9d3c04cd3b7c45a4583ce971ce4bf51966f
Shubhendu Vaid <shubhendu.vaid@bt.com>
2025-01-19 21:17:54 +0000

    fix Task component tests

--- src/components/Task/Task.test.tsx
+++ src/components/Task/Task.test.tsx
@@ -1,29 +1,61 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { vi } from "vitest";
 
 import Task, { PriorityType } from "./Task";
 
 describe("Task Component", () => {
-  const taskProps = {
+  const mockHandleEdit = vi.fn();
+  const mockHandleDelete = vi.fn();
+  const defaultProps = {
+    id: "1",
     title: "Test Task",
     description: "This is a test description",
     priority: "High" as PriorityType,
+    handleEdit: mockHandleEdit,
+    handleDelete: mockHandleDelete,
   };
 
-  it("renders the task title", () => {
-    render(<Task {...taskProps} />);
-    const titleElement = screen.getByText("Test Task");
-    expect(titleElement).toBeInTheDocument();
+  it("renders the component with the correct data", () => {
+    render(<Task {...defaultProps} />);
+
+    expect(screen.getByText("Test Task")).toBeInTheDocument();
+    expect(screen.getByText("This is a test description")).toBeInTheDocument();
+    expect(screen.getByText("High")).toBeInTheDocument();
   });
 
-  it("renders the task description", () => {
-    render(<Task {...taskProps} />);
-    const descriptionElement = screen.getByText("This is a test description");
-    expect(descriptionElement).toBeInTheDocument();
+  it("calls handleEdit when the Edit button is clicked", () => {
+    render(<Task {...defaultProps} />);
+
+    // Click the Edit button
+    const editButton = screen.getByText("Edit");
+    fireEvent.click(editButton);
+
+    // Assert the mock function is called with correct arguments
+    expect(mockHandleEdit).toHaveBeenCalledWith(
+      "1",
+      "Test Task",
+      "This is a test description",
+      "High"
+    );
   });
 
-  it("renders the task priority", () => {
-    render(<Task {...taskProps} />);
-    const priorityElement = screen.getByText("High");
-    expect(priorityElement).toBeInTheDocument();
+  it("calls handleDelete when the Delete button is clicked", () => {
+    render(<Task {...defaultProps} />);
+
+    const deleteButton = screen.getByText("Delete");
+    fireEvent.click(deleteButton);
+
+    expect(mockHandleDelete).toHaveBeenCalledWith("1");
+  });
+
+  it("displays the correct priority class based on the priority level", () => {
+    const { rerender } = render(<Task {...defaultProps} priority="Low" />);
+    expect(screen.getByText("Low")).toBeInTheDocument();
+
+    rerender(<Task {...defaultProps} priority="Medium" />);
+    expect(screen.getByText("Medium")).toBeInTheDocument();
+
+    rerender(<Task {...defaultProps} priority="High" />);
+    expect(screen.getByText("High")).toBeInTheDocument();
   });
 });
